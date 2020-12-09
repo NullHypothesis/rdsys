9bac41304582caa8bc99d04f8c309e34cfa52a4f
Philipp Winter <phw@nymity.ch>
2020-12-09 10:34:40 -0800

    Support logging to file.

--- cmd/backend/main.go
+++ cmd/backend/main.go
@@ -2,7 +2,9 @@ package main
 
 import (
 	"flag"
+	"io"
 	"log"
+	"os"
 
 	"gitlab.torproject.org/tpo/anti-censorship/rdsys/internal"
 )
@@ -10,9 +12,21 @@ import (
 func main() {
 	// TODO: Can we outsource flag parsing and share code across command line
 	// tools?
-	var configFilename string
+	var configFilename, logFilename string
 	flag.StringVar(&configFilename, "config", "", "Configuration file.")
+	flag.StringVar(&logFilename, "log", "", "File to write logs to.")
 	flag.Parse()
+
+	var logOutput io.Writer = os.Stderr
+	if logFilename != "" {
+		logFd, err := os.OpenFile(logFilename, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0600)
+		if err != nil {
+			log.Fatal(err)
+		}
+		logOutput = logFd
+		log.SetOutput(logOutput)
+		defer logFd.Close()
+	}
 
 	if configFilename == "" {
 		log.Fatal("No configuration file provided.  The argument -config is mandatory.")
